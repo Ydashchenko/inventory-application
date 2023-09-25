758e9992d7fe46c7d5da16dc2127faea668b89f4
madjonga <madjonga123@gmail.com>
2023-09-25 20:10:21 +0300

    Connect database

--- app.js
+++ app.js
@@ -7,6 +7,16 @@ const session = require('express-session')
 const app = express()
 const PORT = process.env.PORT || 8000
 
+// database connection
+mongoose.connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true, 
+})
+
+const db = mongoose.connection
+//db.on('error', (error) => console.log(error))
+db.once('open', () => console.log('Connected to the database.'))
+
 app.get('/', (req, res) => {
     res.send('ABOBA')
 })
