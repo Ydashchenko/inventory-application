3118cb3d78b6d3351ea9fce63bba6f17849c09f0
madjonga <madjonga123@gmail.com>
2023-09-25 20:11:00 +0300

    Set middlewares

--- app.js
+++ app.js
@@ -14,8 +14,24 @@ mongoose.connect(process.env.DB_URI, {
 })
 
 const db = mongoose.connection
-//db.on('error', (error) => console.log(error))
+db.on('error', (error) => console.log(error))
 db.once('open', () => console.log('Connected to the database.'))
+
+// Middleware for parsing JSON and handling URL-encoded data
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use(session({
+    secret: 'my secret key',
+    saveUninitialized: true,
+    resave: false,
+}))
+
+app.use((req, res, next) => {
+    res.locals.message = req.session.message
+    delete req.session.message
+    next()
+})
 
 app.get('/', (req, res) => {
     res.send('ABOBA')
